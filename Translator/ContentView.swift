8b46ed0ea3732dd9214c6522518694286fbf5740
Yubo Rao <71145315+matthewtg00@users.noreply.github.com>
2024-03-07 10:05:13 +0800

    Initial Commit

---
+++ Translator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Translator
+//
+//  Created by Yubo Rao on 3/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Translator/TranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslatorApp.swift
+//  Translator
+//
+//  Created by Yubo Rao on 3/7/24.
+//
+
+import SwiftUI
+
+@main
+struct TranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
